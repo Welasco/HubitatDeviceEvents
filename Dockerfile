47eeeba9eca4da6574141393422daed1ff31fe0c
welasco <26201510+Welasco@users.noreply.github.com>
2024-03-01 09:56:00 -0600

    Finish Front End

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Build the application from source
-FROM golang:1.21 AS build-stage
+FROM golang:1.21 AS build-go
 
 WORKDIR /app
 
@@ -9,13 +9,23 @@ RUN go mod download
 COPY . .
 
 RUN CGO_ENABLED=0 GOOS=linux go build -o /hubitatedeviceevents
+#######################################################################
+
+# Build fronend from source
+FROM node:21-bookworm AS build-frontend
+WORKDIR /app
+COPY . .
+RUN cd /app/frontend \
+    && npm install \
+    && npm run build
 
 # Deploy the application binary into a lean image
-FROM alpine AS build-release-stage
+FROM alpine AS build-release
 
 WORKDIR /app
 
-COPY --from=build-stage /hubitatedeviceevents /app/hubitatedeviceevents
+COPY --from=build-go /hubitatedeviceevents /app/hubitatedeviceevents
+COPY --from=build-frontend /app/frontend/dist /app/public
 
 EXPOSE 3000
 
--- transport/trasnport.go
+++ transport/trasnport.go
@@ -1,9 +1,12 @@
 package transport
 
 import (
+	"os"
+
 	"github.com/Welasco/HubitatDeviceEvents/device"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	_ "github.com/joho/godotenv/autoload"
 )
 
 func helloWorld(c *fiber.Ctx) error {
@@ -26,8 +29,28 @@ func setupRoutes(app *fiber.App) {
 // returns a pointer to app
 func Setup() *fiber.App {
 	app := fiber.New()
+
+	if os.Getenv("Environment") == "Dev" {
+		app.Static("/", "./frontend/dist")
+	} else {
+		app.Static("/", "./public")
+	}
 	app.Use(cors.New())
 	setupRoutes(app)
 
 	return app
 }
+
+// _ "github.com/joho/godotenv/autoload"
+// )
+
+// var db database.Database
+
+// var config model.Config
+
+// func LoadConfig() {
+// 	config = model.Config{
+// 		ConnectionString: os.Getenv("ConnectionString"),
+// 		DatabaseType:     os.Getenv("DatabaseType"),
+// 	}
+// }
